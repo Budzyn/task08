fbb8a2fab798f897642d9e79b7ee5d90e9e7590e
Budzyn <black_witcher@ukr.net>
2015-11-03 17:30:28 +0200

    1111: add Guesbook

---
+++ Guestbook/Guestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Guestbook.Models;
+
+
+namespace Guestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        //
+        // GET: /Guestbook/
+        private GuestbookContext db = new GuestbookContext();
+
+
+        public ActionResult Index()
+        {
+            var mostRecentEntries = (from entry in db.Entries orderby entry.DateAdded descending select entry).Take(5);
+            ViewBag.Entries = mostRecentEntries.ToList();
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(GuestbookEntry entry)
+        {
+            entry.DateAdded = DateTime.Now;
+            db.Entries.Add(entry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
